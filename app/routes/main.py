6d0b38cbfa7de6e882c99d7186efc4ca992e93a6
Katharina Burchin <katharinaburchin@MacBook-Pro-von-Katharina.local>
2026-03-26 10:43:18 +0100

    unknown

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,9 @@
+import sqlite3
 from flask import Blueprint, render_template, request
 
 main_bp = Blueprint('main', __name__)
+
+DATABASE = "database.db"
 
 @main_bp.route("/", methods=["GET","POST"])
 def index():
